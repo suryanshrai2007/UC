c7645c8762c2f814dde777cf7c010637d16fc935
suryanshrai2007 <suryanshrai2007@gmail.com>
2026-02-17 21:50:00 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,18 @@
+public class uc3 {
+    public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   *        *      **        *** "));
+        System.out.println(String.join(" "," *   *    *   *    *    *     **     "));
+        System.out.println(String.join(" ","*     *  *     *   *     *   **     "));
+        System.out.println(String.join(" ","*     *  *     *   *    *     **  "));
+        System.out.println(String.join(" ","*     *  *     *   **        *"));
+        System.out.println(String.join(" ","*     *  *     *   *              *  "));
+        System.out.println(String.join(" ","*     *  *     *   *               *     "));
+        System.out.println(String.join(" "," *   *    *    *   *              *       "));
+        System.out.println(String.join(" ","   *        *      *         **    "));
+
+    }
+}
+}
